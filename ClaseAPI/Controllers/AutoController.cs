999bd7ed7a3d48209fadd20ffa4971486550e82b
Joaquín Lopez <joaquinacho@live.com>
2024-08-20 17:46:34 -0300

    Modelo Auto y Controlador con Post

---
+++ ClaseAPI/Controllers/AutoController.cs
@@ -0,0 +1,21 @@
+﻿using ClaseAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoController : ControllerBase
+    {
+
+        // POST api/<AutoController>
+        [HttpPost]
+        public Auto Post(Auto auto)
+        {
+            return auto;
+        }
+
+    }
+}
---
+++ ClaseAPI/Models/Auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClaseAPI.Models
+{
+    public class Auto
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public Motor Motor { get; set; }
+        public Transmision Transmision { get; set; }
+        public string VelocidadMaxima { get; set; }
+        public Confort Confort { get; set; }
+
+    }
+
+    public class Motor
+    {
+        public int Cilindros { get; set; }
+        public double Cilindrada { get; set; }
+        public string Potencia { get; set; }
+        public int ValvulasPorCilindro { get; set; }
+    }
+
+    public class Transmision
+    {
+        public string Traccion { get; set; }
+        public string CajaDeCambios { get; set; }
+    }
+
+    public class Confort
+    {
+        public bool AireAcondicionado { get; set; }
+        public bool Calefaccion { get; set; }
+        public bool LevantaVidrios { get; set; }
+        public string Direccion { get; set; }
+    }
+}
